bba6d4040a4c92866c67a3d8bcc508daa50593f9
joaquin mora <rodolfomorag7@gmail.com>
2026-08-02 23:09:38 -0600

    still in monitor
    why is so difficult the textures and shaders?! well now i have all the animation fixed and i am putting in the screen a texture viewport where you can see what buttons you touch and i am putting the vr system for press the buttons

---
+++ only-five/pantalla_monitor.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Sprite2D3/AnimationPlayer.play("bucle")
+	pass # Replace with function body.
